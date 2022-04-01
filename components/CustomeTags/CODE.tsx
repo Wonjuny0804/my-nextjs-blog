1d334d30b4f96cab3dd554e36980bd73e937688d
Wonjun <wonwonjun@gmail.com>
2022-04-01 23:02:58 +0900

    Complete data-types article

--- components/CustomeTags/CODE.tsx
+++ components/CustomeTags/CODE.tsx
@@ -7,7 +7,7 @@ interface Props {
 const CODE: FC<Props> = ({ children }) => {
   return (
     <code
-      className={`bg-[#0220470d] text-sm lg:text-base whitespace-pre-wrap  border border-[#001b371a] shadow-sm rounded-[4px] px-1 py-0.5`}
+      className={`bg-[#0220470d] text-sm lg:text-base whitespace-pre-wrap border border-[#001b371a] shadow-sm rounded-[4px] px-1 py-0.5`}
     >
       {children}
     </code>
