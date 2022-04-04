33e61f3f943b16f1f730f5720e7cb7a920af3c14
Wonjun <wonwonjun@gmail.com>
2022-04-04 23:30:58 +0900

    Create about page

---
+++ components/about/Top/Top.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+
+const Top: FC = () => {
+  return (
+    <section>
+      <div>
+        <div>
+          <h1>Wonjun Jang</h1>
+          <p>Frontend developer at enkor</p>
+        </div>
+        <p>
+          Currently on a UX journey. Studying and researching sustainable,
+          managable frontend products using React/Next.js
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Top;
