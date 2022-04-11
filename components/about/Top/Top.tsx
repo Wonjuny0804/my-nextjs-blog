aaf778ddd2d1fb557ff46951b29af4fb723f06bc
Wonjun <wonwonjun@gmail.com>
2022-04-11 23:03:10 +0900

    Work on about header styling

--- components/about/Top/Top.tsx
+++ components/about/Top/Top.tsx
@@ -6,7 +6,10 @@ import myImage from "../../../public/assets/wonjun-2.png";
 const Top: FC = React.memo(() => {
   return (
     <section
-      className={`flex flex-col items-center font-workSans lg:flex lg:justify-between`}
+      className={`flex flex-col items-center font-workSans lg:flex 
+        lg:gap-14 lg:flex-row 
+      lg:mt-10 lg:px-10
+      `}
     >
       <div className={`w-[160px] lg:hidden`}>
         <Image
@@ -15,24 +18,26 @@ const Top: FC = React.memo(() => {
           className={`rounded-full`}
         />
       </div>
-      <div className={`px-4`}>
-        <div className={`text-center text-primary-dark`}>
-          <h1 className={`text-2xl font-bold`}>Wonjun Jang</h1>
-          <p className={`text-sm`}>
-            Frontend developer at{" "}
+      <div className={`px-4 lg:px-0`}>
+        <div className={`text-center text-primary-dark lg:text-left`}>
+          <h1 className={`text-2xl font-bold lg:text-3xl`}>Wonjun Jang</h1>
+          <p className={`text-sm lg:text-base lg:mt-2`}>
+            Frontend Developer at{" "}
             <span className={`font-bold`}>enkorwithus</span>
           </p>
         </div>
-        <p className={`text-left text-base mt-4 `}>
-          Currently on a UX journey. <br />
-          Studying and researching sustainable, managable frontend products
-          using React/Next.js
+        <p className={`text-left text-base mt-4 lg:text-base`}>
+          Currently on a UX journey. <br className={`lg:hidden`} />
+          Studying and researching sustainable,{" "}
+          <br className={`hidden lg:block`} />
+          managable frontend products using React/Next.js
         </p>
       </div>
       <div
-        className={`hidden lg:block lg:w-[160px] lg:bg-gradient-to-r lg:from-cyan-500 lg:to-blue-500`}
+        className={`hidden lg:block lg:w-[120px] 
+        lg:bg-gradient-to-r lg:from-cyan-500 lg:to-blue-500`}
       >
-        <Image src={myImage} alt={""} />
+        <Image src={myImage} alt={""} className={`rounded-full`} />
       </div>
     </section>
   );
