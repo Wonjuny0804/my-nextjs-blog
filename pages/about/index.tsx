e2239312da8299c2dec6e6cb8dacdb72b01fb220
Wonjun <wonwonjun@gmail.com>
2022-04-17 23:09:34 +0900

    Add title to about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import dynamic from "next/dynamic";
 import Projects from "../../components/about/Projects/Projects";
+import Head from "next/head";
 
 const Resume = dynamic(() => import("../../components/about/Resume/Resume"));
 const LandingHeader = dynamic(
@@ -11,6 +12,9 @@ const Top = dynamic(() => import("../../components/about/Top/Top"));
 const AboutPage: FC = () => {
   return (
     <>
+      <Head>
+        <title>Frontend dev Wonjun</title>
+      </Head>
       <LandingHeader />
       <main className={`lg:w-[800px] lg:m-auto`}>
         <Top />
