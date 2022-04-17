6e572edec6922ed6120c752b73916c1d4549d3d8
Wonjun <wonwonjun@gmail.com>
2022-04-17 15:36:12 +0900

    Add routing progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNProgress
+        color="#2563eb"
+        options={{
+          showSpinner: false,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
