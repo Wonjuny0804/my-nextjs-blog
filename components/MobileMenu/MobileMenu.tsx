fb1e993533c3a0e8a21ac96308ed3fe6e2eb0981
Wonjun <wonwonjun@gmail.com>
2022-05-16 22:15:15 +0900

    Add mobile menu new design and create PostsPage component

--- components/MobileMenu/MobileMenu.tsx
+++ components/MobileMenu/MobileMenu.tsx
@@ -1,52 +1,65 @@
 import React, { FC } from "react";
 import { MenuIcon } from "@heroicons/react/outline";
-import { Menu } from "@headlessui/react";
+import { Menu, Transition } from "@headlessui/react";
 import Link from "next/link";
 
 const MobileMenu: FC = () => {
   return (
     <div
-      className={`flex font-workSans text-primary-dark items-center lg:hidden`}
+      className={`flex font-workSans text-real-black items-center lg:hidden`}
     >
-      <Menu as="div" className="relative flex items-center text-left">
-        <Menu.Button
-          as="button"
-          className={`focus:outline-none inline-flex w-full 
+      <Menu as="div" className=" flex items-center text-left">
+        {({ open }) => (
+          <>
+            <Menu.Button
+              as="button"
+              className={`focus:outline-none inline-flex w-full 
           justify-center p-2
            text-sm font-medium text-white hover:bg-opacity-30
            focus-visible:ring-2 focus-visible:ring-white 
            focus-visible:ring-opacity-75`}
-        >
-          <MenuIcon
-            className={`w-5 h-5 text-[#000000] font-bold`}
-            aria-hidden="true"
-          />
-        </Menu.Button>
-        <Menu.Items
-          as="ul"
-          className={`absolute shadow-md rounded-lg top-[40px] right-0 flex flex-col gap-3 min-w-[160px] px-5 py-4 bg-white`}
-        >
-          <Menu.Item as="li">
-            <Link href="/">
-              <a>main</a>
-            </Link>
-          </Menu.Item>
-          <Menu.Item as="li">
-            <Link href="/about">
-              <a>about</a>
-            </Link>
-          </Menu.Item>
-          <Menu.Item as="li">
-            <Link href="/">
-              <a>posts</a>
-            </Link>
-          </Menu.Item>
-          <Menu.Item as="li">
-            <Link href="/lab">
-              <a>lab</a>
-            </Link>
-          </Menu.Item>
-        </Menu.Items>
+            >
+              <MenuIcon
+                className={`w-5 h-5 text-[#000000] font-bold`}
+                aria-hidden="true"
+              />
+            </Menu.Button>
+            <Transition
+              show={open}
+              enter={`transition-opacity transition-[height] duration-300`}
+              enterFrom={`opacity-0 h-0`}
+              enterTo={`opacity-100 h-screen`}
+            >
+              <Menu.Items
+                as="ul"
+                className={`absolute top-0 left-0 w-screen h-screen
+           flex flex-col min-w-[160px] py-6 
+          bg-[#f4f4f0]`}
+              >
+                <Menu.Item as="li">
+                  <Link href="/">
+                    <a className={`block w-full border-b py-3 pl-6`}>main</a>
+                  </Link>
+                </Menu.Item>
+                <Menu.Item as="li">
+                  <Link href="/about">
+                    <a className={`block w-full border-b py-3 pl-6`}>about</a>
+                  </Link>
+                </Menu.Item>
+                <Menu.Item as="li">
+                  <Link href="/">
+                    <a className={`block w-full border-b py-3 pl-6`}>posts</a>
+                  </Link>
+                </Menu.Item>
+                <Menu.Item as="li">
+                  <Link href="/lab">
+                    <a className={`block w-full border-b py-3 pl-6`}>lab</a>
+                  </Link>
+                </Menu.Item>
+              </Menu.Items>
+            </Transition>
+          </>
+        )}
       </Menu>
     </div>
   );
---
+++ pages/posts/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostsPage = () => {
+  return <div>index</div>;
+};
+
+export default PostsPage;
