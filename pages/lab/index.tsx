17815e404a281020a4d7ed2e364901647b11611b
Wonjun Jang <wonwonjun@gmail.com>
2022-07-01 12:21:50 +0900

    Write about scene and camera, first draft

--- pages/lab/index.tsx
+++ pages/lab/index.tsx
@@ -1,10 +1,14 @@
+import React from "react";
 import { NextPage } from "next";
 import dynamic from "next/dynamic";
-import React from "react";
-import LabMainPage from "../../components/lab/LabMainPage";
+
+const LabMainPage = dynamic(
+  () => import("../../components/lab/LabMainPage")
+) as any;
+
 const LandingHeader = dynamic(
   () => import("../../components/Landing/LandingHeader")
-);
+) as any;
 
 const LabPage: NextPage = () => {
   return (
