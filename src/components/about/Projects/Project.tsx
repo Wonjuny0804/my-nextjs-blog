6de24bc4032a0409a6d69713ff0f041158c5341a
Wonjun Jang <wonwonjun@gmail.com>
2022-10-17 16:42:50 +0900

    wip - about projects publishing

--- src/components/about/Projects/Project.tsx
+++ src/components/about/Projects/Project.tsx
@@ -22,33 +22,52 @@ const Project: FC<Props> = ({
   link,
 }) => {
   return (
-    <div>
-      <span className={`text-white text-sm font-montserrat`}>
-        {moment(period.from).format("MMM YYYY")} ~{" "}
-        {moment(period.to).format("MMM YYYY")}
-      </span>
-      {link ? (
-        <a target={`_blank`} href={link} className={`hover:underline`}>
+    <div
+      className={`border border-white lg:px-[60px] lg:py-[60px] 
+      `}
+    >
+      <div className={`flex items-center lg:gap-5`}>
+        <span className={`text-[#666666]`}>1</span>
+        <span className={`text-white text-sm font-montserrat`}>
+          {moment(period.from).format("MMM YYYY")} ~{" "}
+          {moment(period.to).format("MMM YYYY")}
+        </span>
+      </div>
+      <div className={`flex items-center lg:gap-5`}>
+        <span className={`text-[#666666]`}>2</span>
+        {link ? (
+          <a target={`_blank`} href={link} className={`hover:underline`}>
+            <h1 className={`font-bold text-xl font-montserrat`}>
+              {projectTitle}
+            </h1>
+          </a>
+        ) : (
           <h1 className={`font-bold text-xl font-montserrat`}>
             {projectTitle}
           </h1>
-        </a>
-      ) : (
-        <h1 className={`font-bold text-xl font-montserrat`}>{projectTitle}</h1>
-      )}
-      <span
-        className={`font-light italic text-[#797979] mb-1 block font-montserrat`}
-      >
-        {team}
-      </span>
-      <p className={`text-white font-montserrat`}>{desc}</p>
-      <h2 className={`font-medium text-lg mt-3 font-notoSans`}>Things I did</h2>
-      <ul
-        className={`list-disc pl-10 font-light font-montserrat flex flex-col gap-1 pt-3`}
-        dangerouslySetInnerHTML={{
-          __html: list,
-        }}
-      ></ul>
+        )}
+      </div>
+      <div className={`flex items-center lg:gap-5`}>
+        <span className={`text-[#666666]`}>3</span>
+        <span
+          className={`font-light italic text-[#797979] mb-1 block font-montserrat`}
+        >
+          {team}
+        </span>
+      </div>
+      <div className={`flex items-center lg:gap-5`}>
+        <span className={`text-[#666666] self-start`}>4</span>
+        <p className={`text-white font-montserrat`}>{desc}</p>
+      </div>
+      <div className={`ml-8 mt-8`}>
+        <h2 className={` text-lg mt-3 font-montserrat`}>Things I did</h2>
+        <ul
+          className={` pl-0 font-light font-montserrat flex flex-col gap-1 pt-3`}
+          dangerouslySetInnerHTML={{
+            __html: list,
+          }}
+        ></ul>
+      </div>
     </div>
   );
 };
--- src/components/about/Projects/Projects.tsx
+++ src/components/about/Projects/Projects.tsx
@@ -1,15 +1,21 @@
 /* eslint-disable react/display-name */
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Project from "./Project";
 import projects from "./projectDatas";
 
 const Projects: FC = React.memo(() => {
+  const [currentProjectIdx, setCurrentProjectIdx] = useState<number>(0);
+
+  const { period, projectTitle, team, desc, list, link } =
+    projects[currentProjectIdx];
+
   return (
     <section className={`mt-20 text-white px-4 lg:p-0 font-montserrat `}>
+      <span className={`block w-full bg-white h-[1px] mb-5`}></span>
       <h1 className={`font-medium text-lg lg:text-2xl lg:text-white`}>
         Projects
       </h1>
-      <ul className={`flex flex-col gap-3 lg:gap-10`}>
+      <ul className={`flex flex-col gap-3 lg:gap-10 mt-20`}>
         {projects.map(({ period, projectTitle, team, desc, list, link }) => (
           <Project
             period={{
@@ -24,6 +30,18 @@ const Projects: FC = React.memo(() => {
             link={link}
           />
         ))}
+        {/* <Project
+          period={{
+            from: period.from,
+            to: period.to,
+          }}
+          projectTitle={projectTitle}
+          team={team}
+          desc={desc}
+          list={list}
+          key={projectTitle}
+          link={link}
+        /> */}
       </ul>
     </section>
   );
